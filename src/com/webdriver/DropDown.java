3319077a046819b3b05d8a276c5615e91a4000b5
sdksd <tanvimeer.tops@gmail.com>
2026-04-13 10:52:09 +0530

    13 april

---
+++ src/com/webdriver/DropDown.java
@@ -0,0 +1,29 @@
+package com.webdriver;
+
+import java.util.Iterator;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class DropDown {
+public static void main(String[] args) throws InterruptedException {
+	System.setProperty("webdriver.chrome.driver", "D:\\Selenium\\chromedriver-win64\\chromedriver.exe");
+	WebDriver driver=new ChromeDriver();
+	driver.get("https://demo.guru99.com/test/newtours/register.php");
+	Thread.sleep(2000);
+	WebElement ele=driver.findElement(By.name("country"));
+	Select s1=new Select(ele);
+	for (int i = 0; i <50; i++) {
+		s1.selectByIndex(i);
+		Thread.sleep(1000);
+	}
+	
+//	Thread.sleep(2000);
+//	s1.selectByValue("INDIA");
+//	Thread.sleep(2000);
+//	s1.selectByVisibleText("IRAN");
+}
+}
